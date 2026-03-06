4b49cb0632c7cfe3bc84833ca2ae365f8619f11c
Kit <4jrkit1@gmail.com>
2026-03-06 11:40:35 +0100

    feat: completed exercise-5

---
+++ assignments/4kit1-glitch/chapter-2/javascript-fundamentals/part-2/exercise-5.js
@@ -0,0 +1,9 @@
+// Short conditional
+const status = age >= 18 ? "Adult" : "Minor";
+
+// Nested ternary
+const category = 
+  score >= 90 ? "A" :
+  score >= 80 ? "B" :
+  score >= 70 ? "C" :
+  "F";
