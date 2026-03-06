939d17a0e2816235520299788ad5c1024ddc682f
Kit <4jrkit1@gmail.com>
2026-03-06 12:12:15 +0100

    feat: completed exercise-2 part-1

---
+++ assignments/4kit1-glitch/chapter-2/javascript-fundamentals/part-3/exercise-2.js
@@ -0,0 +1,13 @@
+// Basic while
+let count = 0;
+while (count < 5) {
+  console.log(count);
+  count++;
+}
+
+// Do-while (runs at least once)
+let num = 10;
+do {
+  console.log(num);
+  num--;
+} while (num > 0);
