bfb1edf26971052bb612e04d3be6edec55a4999f
blackermahn <miibemsimarvin@gmail.com>
2026-03-07 09:56:01 +0100

    Added comparison variables

--- assignments/blackermahn/chapter-2/javascript-fundamentals/my-solutions.js
+++ assignments/blackermahn/chapter-2/javascript-fundamentals/my-solutions.js
@@ -8,3 +8,18 @@ console.log(`Name: ${firstname} ${lastname}`);
 console.log(`Age: ${age}`);
 console.log(`Is Student: ${isStudent}`);
 console.log(`GPA: ${gpa}`);
+
+//exercise 2. arithmetic operators
+const sum = 20 + 2;//gives the sum of two variables
+const difference = 20 - 2;//gives the difference of two variables
+const product = 20 * 2;//gives the product of two variables
+const quotient = 20 / 2;//gives the quotient of two variables. that is: the result of dividing the first variable by the second variable
+const remainder = 20 % 2;//gives the remainder after division of two variables
+
+//comparison operators
+console.log(20 == 20); //true because both sides are equal (but it is loose)
+console.log(20 === 20); //true because both sides are equal (and it is strict)
+console.log(20 > 2); //true because 20 is greater than 2
+console.log(2 < 20); //true because 2 is less than 20
+console.log(20 >= 20); //true because 20 is greater than or equal to 20
+//comparison operators are used mainly to compare the values of two variables. majorly used in if-else statements and loops
