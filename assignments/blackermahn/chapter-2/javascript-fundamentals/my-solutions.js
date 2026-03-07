22da892a40e1c3d23809648fed0c026f531cf5bb
blackermahn <miibemsimarvin@gmail.com>
2026-03-07 09:24:54 +0100

    created my-solutions.js and completed exercise 1 on variables

---
+++ assignments/blackermahn/chapter-2/javascript-fundamentals/my-solutions.js
@@ -0,0 +1,10 @@
+const firstname = "YIBEH";
+const lastname = "Miibemsembom";
+const age = 25;
+const isStudent = true;
+const gpa = 3.8;
+
+console.log(`Name: ${firstname} ${lastname}`);
+console.log(`Age: ${age}`);
+console.log(`Is Student: ${isStudent}`);
+console.log(`GPA: ${gpa}`);
